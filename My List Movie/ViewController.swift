12e7feb94ef60732ca4d19ef2fff13ba0fe1ab39
ilga yulian putra <ilga.putra15@gmail.com>
2020-07-20 08:59:53 +0700

    Initial Commit

---
+++ My List Movie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My List Movie
+//
+//  Created by ilga yulian putra on 20/07/20.
+//  Copyright © 2020 ilga yulian putra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
